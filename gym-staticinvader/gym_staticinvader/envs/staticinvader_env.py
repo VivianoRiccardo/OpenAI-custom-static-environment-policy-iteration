2049fd63690dc3d932e137cb68d72c46a1e96e64
Riccardo Viviano <40870708+VivianoRiccardo@users.noreply.github.com>
2020-11-20 17:48:10 +0100

    Add files via upload

--- gym-staticinvader/gym_staticinvader/envs/staticinvader_env.py
+++ gym-staticinvader/gym_staticinvader/envs/staticinvader_env.py
@@ -3,7 +3,7 @@ import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-# 7X5 grid
+# 6X4 grid
 # We consider a transition function
 # 4 = invaders, 1 = shot, 3 = shuttle, 7 = walls (identifiers)
 # 1 left, 2 right, 3 shot (discrete actions)
@@ -29,12 +29,12 @@ class StaticinvaderEnv(gym.Env):
     # in the middle row there are 3 walls displayed randomly
     # in the last row central column there is our space ship
     def set_init_space(self):
-        self.init_state = np.zeros((7,5))
-        idx1 = np.random.rand(3,5).argsort(1)[:,:3]
-        self.init_state[np.arange(0,3)[:,None],idx1] = 4
-        idx2 = np.random.rand(1,5).argsort(1)[:,:3]
-        self.init_state[np.arange(3,7)[1],idx2] = 7
-        self.init_state[6][2] = 3
+        self.init_state = np.zeros((6,4))
+        idx1 = np.random.rand(2,4).argsort(1)[:,:2]
+        self.init_state[np.arange(0,2)[:,None],idx1] = 4
+        idx2 = np.random.rand(1,4).argsort(1)[:,:2]
+        self.init_state[np.arange(3,4)[:,None],idx2] = 7
+        self.init_state[5][2] = 3
     
     def get_actions(self):
         return action_space
@@ -43,8 +43,8 @@ class StaticinvaderEnv(gym.Env):
         new_state = np.copy(current_state)
       
         #update according to the shots
-        for i in range(6):
-            for j in range(5):
+        for i in range(5):
+            for j in range(4):
                 if i == 0 and new_state[i][j] == 1:
                     new_state[i][j] = 0
                 elif new_state[i][j] == 4 or new_state[i][j] == 7:
@@ -56,21 +56,21 @@ class StaticinvaderEnv(gym.Env):
                       new_state[i+1][j] = 0
                       new_state[i][j] = 1
         idx = -1
-        for i in range(5):
-            if new_state[6][i] == 3:
+        for i in range(4):
+            if new_state[5][i] == 3:
                 idx = i
                 break
                 
         if action == 1:#go to left
-            new_state[6][idx] = 0
-            new_state[6][max(0,idx-1)] = 3
+            new_state[5][idx] = 0
+            new_state[5][max(0,idx-1)] = 3
       
         elif action == 2:#go to right
-            new_state[6][idx] = 0
-            new_state[6][min(4,idx+1)] = 3
+            new_state[5][idx] = 0
+            new_state[5][min(3,idx+1)] = 3
       
         elif action == 3:#shot
-            new_state[5][idx] = 1
+            new_state[4][idx] = 1
         
         else:
             return current_state
@@ -81,16 +81,16 @@ class StaticinvaderEnv(gym.Env):
     def sample_from_action_space(self):
         r = np.random.rand()
         if r < 0.33:
-            return 1
+            return 'left'
         elif r < 0.67:
-            return 2
-        return 3
+            return 'right'
+        return 'shoot'
        
     #returns 1 if we are in terminal state, 0 otherwise
     def is_terminal_state(self,current_state):
         if 4 in current_state:
-            return 0
-        return 1
+            return False
+        return True
        
    # returns a list of all possible states given the old state and an action
     def get_all_possible_new_states(self,old_state, action):
@@ -102,17 +102,36 @@ class StaticinvaderEnv(gym.Env):
             return 1
         return 0
        
-  # compare old state and new state and i will give you the reward (difference of invaders)
+  # compare old state and new state and i will give you the reward (difference of invaders) in this sense is defined as R(s,s')
     def get_reward(self,old_state,new_state):
         unique, counts = np.unique(old_state, return_counts=True)
         d1 = dict(zip(unique, counts))
       
         unique, counts = np.unique(new_state, return_counts=True)
         d2 = dict(zip(unique, counts))
-      
-        return d2[4]-d1[4]
         
-    def step(self, action):
+        if 4 in d1:
+            if 4 in d2:
+                return d1[4]-d2[4]
+            else:
+                return d1[4]
+        return 0
+    
+    
+    def get_reward_s(self,state):
+        reward = 0
+        for i in range(5):
+            for j in range(4):
+                if state[i][j] == 4:
+                    if state[i+1][j] == 1:
+                        reward+=1
+        return reward
+        
+        
+    def step(self, string_action):
+        if string_action not in self.action_space:
+            return np.copy(self.current_state), 0, self.is_terminal_state(self.current_state), {}
+        action = self.action_space[string_action]
         old_state = self.current_state
         new_states = self.get_all_possible_new_states(old_state,action)
         l1 = []
@@ -121,7 +140,6 @@ class StaticinvaderEnv(gym.Env):
             l1.append(i)
             l2.append(self.get_probability_of_new_state(new_states[i],old_state,action))
         new_state = new_states[np.random.choice(l1,p=l2)]
-        print(new_state)
         self.current_state = new_state
         reward = self.get_reward(old_state,self.current_state)
         done = self.is_terminal_state(self.current_state)
@@ -132,10 +150,8 @@ class StaticinvaderEnv(gym.Env):
     def reset(self):
         self.current_state = np.copy(self.init_state)
     def render(self, mode='human'):
-        for i in range(7):
-            for i in range(5):
-                print(str(self.current_state[i][j]) + " ")
-            print("\n")
+        print(self.current_state)
+        print("\n")
      
     def close(self):
         pass
---
+++ policy_iteration.py
@@ -0,0 +1,154 @@
+import gym
+import numpy as np
+import time
+import pickle
+
+def save_d(filename,d):
+    with open(filename, 'wb') as handle:
+        pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_d(filename,d):
+    with open(filename, 'rb') as handle:
+        return pickle.load(handle)
+    
+def from_nparray_to_str(array):
+    a = array.flatten()
+    a_str = ','.join(str(x) for x in a)
+    return a_str
+
+def from_string_to_nparray(string):
+    a = np.array([float(x) for x in string.split(',')])
+    return np.reshape(a,(-1,4))
+        
+
+# returns a dictionary with keys: all possible states, value: a list with initialized utility function per state (0), initialized policy per state (a random action)
+# not recursive because recursion exceeds
+def get_states_recursive(d, env, state):
+    if env.is_terminal_state(state):
+        return d
+    for i in env.action_space.keys():
+        new_states = env.get_all_possible_new_states(state, env.action_space[i])
+        for j in new_states:
+            string = from_nparray_to_str(j)
+            if string not in d:
+                d[string] = [0,env.sample_from_action_space()]
+                d = get_states_recursive(d,env,j)
+                
+    return d      
+      
+# returns a dictionary with keys: all possible states, value: a list with initialized utility function per state (0), initialized policy per state (a random action)
+# not recursive because recursion exceeds
+def get_states_iterative(d, env, state):
+    if env.is_terminal_state(state):
+        return d
+    l = [state]
+    num = 0
+    while(len(l) > 0):
+        num+=len(l)#print this number to debug
+        l2 = []
+        for s in l:
+            if env.is_terminal_state(s):
+                continue
+            
+            for i in env.action_space.keys():
+                new_states = env.get_all_possible_new_states(s, env.action_space[i])
+                
+
+                for j in new_states:
+                    string = from_nparray_to_str(j)
+                    if string not in d:
+                        l2.append(j)
+                        d[string] = [0,env.sample_from_action_space()]
+        l = l2
+                
+    return d        
+        
+def policy_evaluation(env, d, discount_factor):
+    flag = True
+    while(flag):
+        flag = False
+        for i in d.keys():
+            state = from_string_to_nparray(i)
+            if env.is_terminal_state(state):
+                continue
+            new_states = env.get_all_possible_new_states(state, env.action_space[d[i][1]])
+            sum_over_probability = 0.
+            for j in new_states:
+                sum_over_probability += env.get_probability_of_new_state(j,state,env.action_space[d[i][1]])*d[from_nparray_to_str(j)][0]
+            value = env.get_reward_s(state) + sum_over_probability*discount_factor
+            if value != d[i][0]:
+                flag = True
+            d[i][0] = value
+
+# returns a dictionary with keys: all possible states, value: a list with utility function per state , policy per state
+def policy_iteration(env):
+    d = {}
+    
+    init_state_str = from_nparray_to_str(env.init_state)
+    d[init_state_str] = [0,env.sample_from_action_space()]
+    d = get_states_iterative(d,env,env.init_state)
+    discount_factor = 0.5
+    
+    for _ in range(4):
+        print("policy evaluation")
+        policy_evaluation(env,d,discount_factor)
+        print("policy improvement")
+        for i in d:
+            
+            state = from_string_to_nparray(i)
+            if env.is_terminal_state(state):
+                continue
+            
+            maximum = -1
+            optimal_action = "None"
+            
+            for j in env.action_space.keys():
+                new_states = env.get_all_possible_new_states(state, env.action_space[j])
+                sum_over_probability = 0.
+                for z in new_states:
+                    sum_over_probability += env.get_probability_of_new_state(z,state,env.action_space[j])*d[from_nparray_to_str(z)][0]
+                if sum_over_probability > maximum:
+                    maximum = sum_over_probability
+                    optimal_action = j
+                    
+            new_states = env.get_all_possible_new_states(state, env.action_space[d[i][1]])
+            sum_over_probability = 0.
+            for j in new_states:
+                sum_over_probability += env.get_probability_of_new_state(j,state,env.action_space[d[i][1]])*d[from_nparray_to_str(j)][0]
+            
+            if maximum > sum_over_probability:
+                d[i][1] = optimal_action
+                
+    return d
+
+def first_quest():
+    env = gym.make('gym_staticinvader:staticinvader-v0')
+    np.random.seed()
+    for _ in range(5):
+        print("old state")
+        env.render()#old_state
+        action = env.sample_from_action_space()
+        new_state, reward, terminal, info = env.step(action)
+        print("action: "+action)#action
+        print("new state")
+        print(new_state)#new state
+        print("reward: "+str(reward))#reward
+        print("is terminal: "+str(terminal))#is terminal
+        print('Probability of being in this new state given that action: '+str(info['probability of being in this state given the old state and the action taken']))#probability distribution
+             
+def second_quest():
+    env = gym.make('gym_staticinvader:staticinvader-v0')
+    np.random.seed()
+    d = policy_iteration(env)
+    env.reset()
+    while(not env.is_terminal_state(env.current_state)):
+        env.render()
+        env.step(d[from_nparray_to_str(env.current_state)][1])
+        time.sleep(0.3)
+    print("The game ended")
+            
+if __name__ == "__main__":
+    print("First quest:")
+    first_quest()
+    print("Second quest:")
+    second_quest()
